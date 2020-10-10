2665e1706f2369b458b3e4eb81fe4c59cac9d758
WrenStormMijiu <1060162567@qq.com>
2020-10-11 01:41:14 +0800

    jiage yyds

---
+++ Assets/Scripts/WaterManager.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaterManager : MonoBehaviour
+{
+    public static WaterManager Instance { get; private set;}
+
+    public float water;
+
+    private void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+}
