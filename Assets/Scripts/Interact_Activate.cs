66687f0746b3bd71e9f8c30a38f77b45e43991d1
Ruoyu Jia (Alvin) <55768236+AlvinJIa@users.noreply.github.com>
2020-10-13 14:34:12 +0800

    Add files via upload

--- Assets/Scripts/Interact_Activate.cs
+++ Assets/Scripts/Interact_Activate.cs
@@ -4,10 +4,6 @@ using UnityEngine;
 using UnityEngine.UI;
 using System;
 using UnityStandardAssets.Characters.FirstPerson;
-<<<<<<< HEAD
-=======
-using System.Diagnostics;
->>>>>>> 01eb0a4df9097700877b5eb298d229353c8e6739
 
 public class Interact_Activate : MonoBehaviour
 {
@@ -21,7 +17,6 @@ public class Interact_Activate : MonoBehaviour
     private bool Ifcollide;
     private Animator animator;
     private GameObject WaterMinusText;
-<<<<<<< HEAD
     private GameObject CleanWaterAmountText;
     private GameObject DirtyWaterAmountText;
     private GameObject CleanWaterAmountImage;
@@ -30,10 +25,6 @@ public class Interact_Activate : MonoBehaviour
     private static List<AnimatorClipInfo> clipList = new List<AnimatorClipInfo>();
 
     private bool checkonce = true;
-=======
-    private GameObject WaterAmountText;
-    private static List<AnimatorClipInfo> clipList = new List<AnimatorClipInfo>();
->>>>>>> 01eb0a4df9097700877b5eb298d229353c8e6739
     QuestManager questmanager;
     //[SerializeField] private Image healthImage;
     private AudioSource audio;
@@ -47,7 +38,6 @@ public class Interact_Activate : MonoBehaviour
     {
         InteractiveMat.SetFloat("_Outline", 0);
         Ifcollide = false;
-<<<<<<< HEAD
 
         CleanWaterAmountText = GameObject.Find("CleanWaterAmount");
         DirtyWaterAmountText = GameObject.Find("DirtyWaterAmount");
@@ -58,19 +48,12 @@ public class Interact_Activate : MonoBehaviour
         DirtyWaterAmountImage = GameObject.Find("ContainerDirty_Green");
         HealthImage = GameObject.Find("HealthBar_Red");
 
-=======
-        WaterAmountText = GameObject.Find("WaterAmount");
-        WaterAmountText.GetComponent<Text>().text = "" + WaterManager.Instance.water;
->>>>>>> 01eb0a4df9097700877b5eb298d229353c8e6739
         WaterMinusText = GameObject.Find("MinusWater");
         WaterMinusText.GetComponent<Text>().text = "";
         animator = UI.GetComponent<Animator>();
         audio = GetComponent<AudioSource>();
-<<<<<<< HEAD
 
 
-=======
->>>>>>> 01eb0a4df9097700877b5eb298d229353c8e6739
     }
     private void OnTriggerEnter(Collider collision)
     {
@@ -94,123 +77,40 @@ public class Interact_Activate : MonoBehaviour
         {
             if (InteractiveObject.name == "InteractiveObject01_WashTab")
             {
-<<<<<<< HEAD
                 usewater(5, 1);
-=======
-                if (WaterManager.Instance.water >= 5)
-                {
-                    WaterMinusText.GetComponent<Text>().text = "- 5L";
-                    WaterManager.Instance.water -= 5;
-                    //print(WaterManager.Instance.water);
-                    WaterManager.Instance.Washhands = true;
-                    questmanager.CheckWinning();
-                    WaterAmountText.GetComponent<Text>().text = "" + WaterManager.Instance.water;
-                }
-                else
-                {
-                    WaterMinusText.GetComponent<Text>().text = "Running out";
-                }
-
->>>>>>> 01eb0a4df9097700877b5eb298d229353c8e6739
                 GameObject Task = GameObject.Find("Task05");
                 GenerateStrike(Task);
             }
             if(InteractiveObject.name == "InteractiveObject02_Glass")
             {
-<<<<<<< HEAD
                 usewater(3, 2);
-=======
-                if (WaterManager.Instance.water >= 3)
-                {
-                    WaterMinusText.GetComponent<Text>().text = "- 3L";
-                    WaterManager.Instance.water -= 3;
-                    //print(WaterManager.Instance.water);
-                    WaterManager.Instance.Drink = true;
-                    questmanager.CheckWinning();
-                    WaterAmountText.GetComponent<Text>().text = "" + WaterManager.Instance.water;
-                }
-                else
-                {
-                    WaterMinusText.GetComponent<Text>().text = "Running out";
-                }
->>>>>>> 01eb0a4df9097700877b5eb298d229353c8e6739
 
                 GameObject Task = GameObject.Find("Task01");
                 GenerateStrike(Task);
             }
             if (InteractiveObject.name == "InteractiveObject03_BathTab")
             {
-<<<<<<< HEAD
                 usewater(50, 3);
-=======
-                if (WaterManager.Instance.water >= 50)
-                {
-                    //print(WaterManager.Instance.water);
-                    WaterMinusText.GetComponent<Text>().text = "- 50L";
-                    WaterManager.Instance.water -= 50;
-                    //print(WaterManager.Instance.water);
-                    WaterManager.Instance.Takebath = true;
-                    questmanager.CheckWinning();
-                    WaterAmountText.GetComponent<Text>().text = "" + WaterManager.Instance.water;
-                }
-                else
-                {
-                    WaterMinusText.GetComponent<Text>().text = "Running out";
-                }
->>>>>>> 01eb0a4df9097700877b5eb298d229353c8e6739
 
                 GameObject Task = GameObject.Find("Task04");
                 GenerateStrike(Task);
             }
             if (InteractiveObject.name == "InteractiveObject04_WashMachine")
             {
-<<<<<<< HEAD
                 usewater(60, 4);
-=======
-                if (WaterManager.Instance.water >= 60)
-                {
-                    WaterMinusText.GetComponent<Text>().text = "- 60L";
-                    WaterManager.Instance.water -= 60;
-                    //print(WaterManager.Instance.water);
-                    WaterManager.Instance.Washmachine = true;
-                    questmanager.CheckWinning();
-                    WaterAmountText.GetComponent<Text>().text = "" + WaterManager.Instance.water;
-                }
-                else
-                {
-                    WaterMinusText.GetComponent<Text>().text = "Running out";
-                }
->>>>>>> 01eb0a4df9097700877b5eb298d229353c8e6739
 
                 GameObject Task = GameObject.Find("Task03");
                 GenerateStrike(Task);
             }
             if (InteractiveObject.name == "InteractiveObject05_Toilet")
             {
-<<<<<<< HEAD
                 usewater(6, 5);
-=======
-                if (WaterManager.Instance.water >= 6)
-                {
-                    WaterMinusText.GetComponent<Text>().text = "- 6L";
-                    WaterManager.Instance.water -= 6;
-                    //print(WaterManager.Instance.water);
-                    WaterManager.Instance.Washhands = true;
-                    questmanager.CheckWinning();
-                    WaterAmountText.GetComponent<Text>().text = "" + WaterManager.Instance.water;
-                }
-                else
-                {
-                    WaterMinusText.GetComponent<Text>().text = "Running out";
-                }
->>>>>>> 01eb0a4df9097700877b5eb298d229353c8e6739
 
                 GameObject Task = GameObject.Find("Task02");
                 GenerateStrike(Task);
             }
             WaterMinusText.SetActive(true);
             PlayAnim(animator);
-<<<<<<< HEAD
             //audio.Play(0);
             Ifcollide = false;
         }
@@ -229,11 +129,6 @@ public class Interact_Activate : MonoBehaviour
             }
             checkonce = false;
         }
-=======
-            audio.Play(0);
-            Ifcollide = false;
-        }
->>>>>>> 01eb0a4df9097700877b5eb298d229353c8e6739
     }
     private void GenerateStrike(GameObject Task)
     {
@@ -257,7 +152,6 @@ public class Interact_Activate : MonoBehaviour
 
         WaterMinusText.SetActive(false);
     }
-<<<<<<< HEAD
 
     void usewater(int usage, int interact)
     {
@@ -329,6 +223,4 @@ public class Interact_Activate : MonoBehaviour
         DirtyWaterAmountImage.GetComponent<Image>().fillAmount = WaterManager.Instance.dirtywater / 150;
         HealthImage.GetComponent<Image>().fillAmount = WaterManager.Instance.health / 100;
     }
-=======
->>>>>>> 01eb0a4df9097700877b5eb298d229353c8e6739
 }
